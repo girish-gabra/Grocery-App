d749f06a363be8953a5d0a5e8f61d4ea1d13c665
girish-gabra <girish.gabra10@gmail.com>
2017-05-30 20:05:32 -0700

    Added service for groceryList Items

--- js/app.js
+++ js/app.js
@@ -17,13 +17,10 @@ app.config(function($routeProvider){
         })
 })
 
-
-app.controller("HomeController", ["$scope", function($scope) {
-    $scope.appTitle = "Grocery List";
-}]);
-
-app.controller("GroceryListItemsController", ["$scope", function($scope){
-    $scope.groceryItems = [
+// add a service
+app.service("GroceryService",function(){
+    var groceryService = []
+    groceryService.groceryItems = [
         {itemName: 'milk', date: '2014-10-01'},
         {itemName: 'cookies', date: '2014-10-01'},
         {itemName: 'ice cream', date: '2014-10-02'},
@@ -32,6 +29,15 @@ app.controller("GroceryListItemsController", ["$scope", function($scope){
         {itemName: 'bread', date: '2014-10-03'},
         {itemName: 'eggs', date: '2014-10-04'},
         {itemName: 'tortillas', date: '2014-10-04'}
-    ]
+    ];    
+    return groceryService
+});
 
+app.controller("HomeController", ["$scope", function($scope) {
+    $scope.appTitle = "Grocery list";
+}]);
+
+app.controller("GroceryListItemsController",  ["$scope","GroceryService",  function($scope,GroceryService){
+    $scope.groceryItems = GroceryService.groceryItems
+        
 }]);
