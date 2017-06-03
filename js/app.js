fa10a29e9890f260befeaf7046b87d3010c74698
girish-gabra <girish.gabra10@gmail.com>
2017-06-03 14:07:07 -0700

    Added 'Edit Item' functionality

--- js/app.js
+++ js/app.js
@@ -6,11 +6,15 @@ app.config(function($routeProvider){
     $routeProvider
         .when("/", {
             templateUrl: "views/groceryList.html",
-            controller:  "GroceryListItemsController"
+            controller:  "HomeController"
         })
         .when("/addItem", {
             templateUrl: "views/addItem.html",
-            controller:  "GroceryListItemsController"
+            controller:  "GroceryListItemController"
+        })
+        .when("/addItem/edit/:id", {
+            templateUrl: "views/addItem.html",
+            controller:  "GroceryListItemController"
         })
         .otherwise({
             redirectTo: "/"
@@ -30,6 +34,15 @@ app.service("GroceryService",function(){
         {id:7, completed:true, itemName: 'eggs', date: '2014-10-04'},
         {id:8, completed:true, itemName: 'tortillas', date: '2014-10-04'}
     ];    
+    console.log('inside groceryService');
+    groceryService.findByID = function(id){
+        for(var item in groceryService.groceryItems)
+        {
+            if(groceryService.groceryItems[item].id == id){
+                return groceryService.groceryItems[item];
+            }
+        }
+    }
 
     groceryService.getNewID = function(){
         if(groceryService.newID){
@@ -44,24 +57,45 @@ app.service("GroceryService",function(){
     }
 
     groceryService.save = function(entry){
-        entry.id = groceryService.getNewID();
-        groceryService.groceryItems.push(entry);
+        
+        var updatedItem = groceryService.findByID(entry.id);
+
+        if(updatedItem){
+           updatedItem.completed = entry.completed; 
+           updatedItem.itemName = entry.itemName;
+           updatedItem.date = entry.date;     
+        }
+        else{
+            entry.id = groceryService.getNewID();    
+            groceryService.groceryItems.push(entry);
+        }
+        
+        
     }
 
     return groceryService
 });
 
-app.controller("HomeController", ["$scope", function($scope) {
+app.controller("HomeController", ["$scope", "GroceryService", function($scope,GroceryService) {
     $scope.appTitle = "Grocery list";
+    //console.log('inside home controller');
+     $scope.groceryItems = GroceryService.groceryItems;
 }]);
 
-app.controller("GroceryListItemsController",  ["$scope","$location", "GroceryService",  function($scope,$location,GroceryService){
-    $scope.groceryItems = GroceryService.groceryItems;
+app.controller("GroceryListItemController",  ["$scope","$routeParams","$location", "GroceryService",  function($scope,$routeParams,$location,GroceryService){
+   
+    if(!$routeParams.id){   // if no id is passed then create new item else edit the current item
+        $scope.groceryItem = {id:0, completed:false, itemName:"",date: new Date()} ;    
+    }else{
+        // clone current object as if edit original and press cancel it will still show changed value because of
+        // two way data binding
+        $scope.groceryItem = _.clone(GroceryService.findByID(parseInt($routeParams.id)));   
+        console.log($scope.groceryItem);
+    } 
     
-    $scope.groceryItem = {id:7, completed:true, itemName:"",date: new Date()} ;
     $scope.save = function(){
         GroceryService.save($scope.groceryItem);
         $location.path("/");
     }
-    console.log($scope.groceryItems);
+    //console.log($scope.groceryItems);
 }]);
